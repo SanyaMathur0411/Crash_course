f0a866adc17438dd4b57292e383efe0173aa68d8
SanyaMathur0411 <113873545+SanyaMathur0411@users.noreply.github.com>
2023-03-15 07:43:05 +0530

    Create average_of_elements.c

---
+++ average_of_elements.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{    
+    int array[5];
+     int sum=0;
+     
+     printf("Enter array\n");
+    for (int i=0;i<5;i++)//to read an array
+    {
+        scanf("%d",&array[i]);
+        
+    }
+  for (int j=0;j<5;j++)//loop to sum elements
+  {
+      sum+=array[j];
+  }
+  float average=(float)sum/5;//calculates average
+  printf("average of elements=%f",average);
+}
+
+    
